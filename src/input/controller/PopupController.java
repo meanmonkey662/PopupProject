5442c9f1a49bbfe3c7389134ea2378e3b063fca1
meanmonkey662 <kkoc6943@740s-pl-132.local>
2015-09-24 13:56:00 -0600

    asdf

--- src/input/controller/PopupController.java
+++ src/input/controller/PopupController.java
@@ -1,11 +1,57 @@
 package input.controller;
 
+import input.view.PopupDisplay;
+import input.model.Thing;
+
 public class PopupController
 {
-
+	private PopupDisplay myPopups;
+	private Thing myThing;
+	
+	public PopupController()
+	{
+		myPopups = new PopupDisplay();
+	}
 	
 	public void start()
 	{
+		String name = myPopups.getAnswers("Type in your name");
 		
+		myPopups.displayResponse("You typed in  " + name);
+		
+		int age = myPopups.getAnswers("Type in your age");
+		myPopups.displayResponse("You typed in " + age);
+		
+		double weight = myPopups.getAnswer("Type in your age")
+		myPopups.displayResponse("You typed in " + weight);
+		
+		myThing = Thing(name, age, weight);
+	}
+	
+	private boolean isInteger(String input)
+	{
+		booean isInt = false;
+		
+		try
+		{
+			int validInteger = Integer.parseInt(input);
+			isInt = true;
+		}
+		catch(NumberFormatException error)
+		{
+			catch(NumberFormatException error)
+			{
+				myPopups.displayResponse("You did not type in a valid integer :(");
+			}
+			
+			return isInt;
+		}
+		
+		private boolean isDouble(String input)
+		{
+			boolean isDouble = false;
+			
+			try 
+		}
 	}
 }
